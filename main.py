d7ce941490738212d8863de9572c60112b7d70cc
Mohammad Nurrokim <mohammadnurrokim@gmail.com>
2021-07-16 06:24:12 +0700

    Encapsulation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from geometri.bangun_ruang import BangunRuang
 from geometri.square import PersegiPanjang
 from geometri.triangle import Segitiga
 
@@ -9,3 +10,8 @@ print(p1.hitung_luas())
 s1 = Segitiga(4, 2)
 print(s1.info())
 print(s1.hitung_luas())
+
+print('\nMencoba membuat object dari kelas BangunRuang')
+b1 = BangunRuang()
+print(b1.info())
+print(b1.hitung_luas())
